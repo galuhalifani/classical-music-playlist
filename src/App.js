198d5248de32ba115b57964cf54aa9209b3b940e
Galuh Alifani <galuh.adika@gmail.com>
2021-07-06 14:46:00 +0700

    search bar event handler

--- src/App.js
+++ src/App.js
@@ -1,55 +1,114 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import PlaylistCard from './views/PlaylistCard.js'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import Loader from "./components/Loader.js";
 
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      appTitle: "Your Classical Music Playlist",
-      playlists: []
-    }
-  }
+export default function App() {
+  const [appTitle] = useState("Your Classical Music Playlist")
+  const [playlists, setPlaylists] = useState([])
+  const [renderedPlaylist, setRenderedPlaylist] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [searchBar, setSearchBar] = useState('')
 
-  componentDidMount() {
+  useEffect(() => {
+    setLoading(true)
     fetch("https://v1.nocodeapi.com/galuhalifani/spotify/rGPSdDBWgbWtmwxO/browse/categoryPlaylist?category_id=classical")
     .then(response => response.json())
     .then(data => {
-      this.setState({playlists: data.playlists.items})
+      setPlaylists(data.playlists.items)
+      setRenderedPlaylist(data.playlists.items)
       console.log(data.playlists.items)
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err)
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  function searchPlaylist(event) {
+    event.preventDefault()
+    let newPlaylist = playlists.filter(list => list.name.toLowerCase().includes(searchBar.toLowerCase()))
+    // console.log(newPlaylist, 'FILTERED ARRAY')
+    setRenderedPlaylist(newPlaylist)
   }
 
-  render () {
-    return (
-      <div>
+  function searchBarChange(event) {
+    setSearchBar(event.target.value)
+  }
+
+  return (
+    <div>
       <Navbar id='navbar' className='navbar-expand-lg navbar-dark'>
-        <Navbar.Brand className="navbar-brand" href="/">{this.state.appTitle}</Navbar.Brand>
+        <Navbar.Brand className="navbar-brand" href="/">{appTitle}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" style={{marginLeft: '3%'}}>
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">My Playlists</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
+          <Nav className="mr-auto" style={{marginLeft: '3%'}}>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">My Playlists</Nav.Link>
+          </Nav>
+        <Form inline onSubmit={searchPlaylist}>
+          <FormControl type="text" placeholder="Search Playlist Title" name='search' className="mr-sm-2" onChange={searchBarChange}/>
+          <Button type='submit' variant="outline-success">Search</Button>
         </Form>
       </Navbar.Collapse>
       </Navbar>
 
       <div className='main_content'>
-        <PlaylistCard playlists={this.state.playlists}/>
+        {
+          loading 
+          ? <Loader/>
+          : <PlaylistCard playlists={renderedPlaylist}/>
+        }
       </div>
-      </div>
-    )
-  }  
+
+    </div>
+  )
 }
 
-export default App;
+// class App extends React.Component {
+//   constructor () {
+//     super()
+//     this.state = {
+//       appTitle: "Your Classical Music Playlist",
+//       playlists: []
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch("https://v1.nocodeapi.com/galuhalifani/spotify/rGPSdDBWgbWtmwxO/browse/categoryPlaylist?category_id=classical")
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({playlists: data.playlists.items})
+//       console.log(data.playlists.items)
+//     })
+//     .catch(err => console.log(err))
+//   }
+
+//   render () {
+//     return (
+//       <div>
+//       <Navbar id='navbar' className='navbar-expand-lg navbar-dark'>
+//         <Navbar.Brand className="navbar-brand" href="/">{this.state.appTitle}</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//         <Nav className="mr-auto" style={{marginLeft: '3%'}}>
+//         <Nav.Link href="#home">Home</Nav.Link>
+//         <Nav.Link href="#link">My Playlists</Nav.Link>
+//         </Nav>
+//         <Form inline>
+//           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+//           <Button variant="outline-success">Search</Button>
+//         </Form>
+//       </Navbar.Collapse>
+//       </Navbar>
+
+//       <div className='main_content'>
+//         <PlaylistCard playlists={this.state.playlists}/>
+//       </div>
+//       </div>
+//     )
+//   }  
+// }
+
+// export default App;
--- src/views/PlaylistCard.js
+++ src/views/PlaylistCard.js
@@ -1,30 +1,56 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import '../App.css';
 
-class PlaylistCard extends React.Component {
-    seePlaylist(e, id) {
+export default function PlaylistCard(props) {
+    function seePlaylist(e, id) {
         e.preventDefault()
         console.log(id)
     }
 
-    render () {
-        return (
-            <div className='row' style={{paddingLeft:'5%', paddingRight: '5px', paddingBottom: '10px'}}>
-                { this.props.playlists.map(playlist => 
-                    <Card className='card m-3' key={playlist.id} style={{width: '18rem', backgroundColor: 'black', border: '2px solid #eae0aa', marginRight:'5px', marginBottom:'10px'}}>
-                        <img className='class="card-img-top' src={playlist.images[0].url} alt="Playlist Poster"/>
-                        <Card.Body>
-                            <Card.Title>{playlist.name}</Card.Title>
+    return (
+        <div className='row' style={{paddingLeft:'5%', paddingRight: '5px', paddingBottom: '10px'}}>
+            { props.playlists.map(playlist => 
+                <Card className='card m-3' key={playlist.id} style={{width: '18rem', backgroundColor: 'black', border: '2px solid #eae0aa', marginRight:'5px', marginBottom:'10px'}}>
+                    <img className='class="card-img-top' src={playlist.images[0].url} alt="Playlist Poster"/>
+                    <Card.Body>
+                        <Card.Title>{playlist.name}</Card.Title>
 
-                            <Card.Text className='mb-0 mt-0'><b>Total Tracks: {playlist.tracks.total}</b></Card.Text><br />
-                            <Card.Text className="overflow-ellipsis">{playlist.description}</Card.Text>
+                        <Card.Text className='mb-0 mt-0'><b>Total Tracks: {playlist.tracks.total}</b></Card.Text><br />
+                        <Card.Text className="overflow-ellipsis">{playlist.description}</Card.Text>
 
-                            <Card.Link onClick={(e) => this.seePlaylist(e, playlist.id)} className="btn btn-primary">See Playlist</Card.Link>
-                        </Card.Body>
-                    </Card> )}
-            </div>
-        )
-    }
+                        <Card.Link onClick={(e) => seePlaylist(e, playlist.id)} className="btn btn-primary">See Playlist</Card.Link>
+                    </Card.Body>
+                </Card> 
+            )}
+        </div>
+    )
 }
 
-export default PlaylistCard
+// class PlaylistCard extends React.Component {
+//     seePlaylist(e, id) {
+//         e.preventDefault()
+//         console.log(id)
+//     }
+
+//     render () {
+//         return (
+//             <div className='row' style={{paddingLeft:'5%', paddingRight: '5px', paddingBottom: '10px'}}>
+//                 { this.props.playlists.map(playlist => 
+//                     <Card className='card m-3' key={playlist.id} style={{width: '18rem', backgroundColor: 'black', border: '2px solid #eae0aa', marginRight:'5px', marginBottom:'10px'}}>
+//                         <img className='class="card-img-top' src={playlist.images[0].url} alt="Playlist Poster"/>
+//                         <Card.Body>
+//                             <Card.Title>{playlist.name}</Card.Title>
+
+//                             <Card.Text className='mb-0 mt-0'><b>Total Tracks: {playlist.tracks.total}</b></Card.Text><br />
+//                             <Card.Text className="overflow-ellipsis">{playlist.description}</Card.Text>
+
+//                             <Card.Link onClick={(e) => this.seePlaylist(e, playlist.id)} className="btn btn-primary">See Playlist</Card.Link>
+//                         </Card.Body>
+//                     </Card> )}
+//             </div>
+//         )
+//     }
+// }
+
+// export default PlaylistCard
