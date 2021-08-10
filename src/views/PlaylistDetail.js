ff740d9845351b7ddddaffd221e1b7c8403ba257
Galuh Alifani <galuh.adika@gmail.com>
2021-08-10 21:37:58 +0700

    display adjustmnet

--- src/views/PlaylistDetail.js
+++ src/views/PlaylistDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Card from 'react-bootstrap/Card'
+import { Card, Row, Col } from 'react-bootstrap'
 import Loader from "../components/Loader.js";
 import Error from "../components/Error.js";
 import { useParams } from "react-router-dom";
@@ -14,22 +14,6 @@ export default function PlaylistDetail(props) {
     const error = select(state => state.playlists.error)
     const loading = select(state => state.playlists.loading)
     const playlist = select(state => state.playlists.playlistDetail)
-
-    // useEffect(() => {
-    //     props.changeActivePage('details')
-    //     setLoading(true)
-    //     fetch(`https://v1.nocodeapi.com/galuhalifani/spotify/rGPSdDBWgbWtmwxO/playlists?id=${id}`)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         dispatch(setPlaylistDetail(data))
-    //         console.log(`BERHASIL FETCH DETAIL`)
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //         setError(true)
-    //     })
-    //     .finally(() => setLoading(false))
-    // }, [])
 
     useEffect(() => {
         props.changeActivePage('details')
@@ -47,7 +31,8 @@ export default function PlaylistDetail(props) {
             loading ? <Loader/> : error ? <Error/> : playlist.id ? 
         
             <div className='playlistDetail m-3' style={{paddingLeft:'5%', paddingTop: '2%', paddingRight: '5%', paddingBottom: '10px'}}>
-            <Card style={{backgroundColor: 'black', marginRight:'25px', marginBottom:'10px', width: '30%'}}>
+
+            <Card className='playlistDetailCard' style={{backgroundColor: 'black', marginRight:'25px', marginBottom:'10px', width: '30%'}}>
                 <img className='class="card-img-top' src={playlist.images[0].url} alt="Playlist Poster"/>
                 <Card.Body>
                     <Card.Title>{playlist.name}</Card.Title>
@@ -57,10 +42,11 @@ export default function PlaylistDetail(props) {
 
                 </Card.Body>
             </Card>
-            <div className="holds-the-iframe" style={{width:"70%", height: '100vh'}}>
-            <iframe title="`{playlist.id}`" src={`https://open.spotify.com/embed/playlist/${playlist.id}`} onLoad="$('.holds-the-iframe').css('background-image', 'none');" width= '100%' height='550px' frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-            </div>
-            </div>
+
+                <div className="holds-the-iframe" style={{width:"70%", height: '100vh'}}>
+                <iframe title="`{playlist.id}`" src={`https://open.spotify.com/embed/playlist/${playlist.id}`} onLoad="$('.holds-the-iframe').css('background-image', 'none');" width= '100%' height='550px' frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>
+            </div>            
             :
             null
         }
